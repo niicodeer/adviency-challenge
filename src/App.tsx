51cfd6b595ebf4fc2ca7be8cad4bb001b6d82dda
Nico Radin <97641886+niicodeer@users.noreply.github.com>
2022-12-06 10:10:33 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Regalos:</h1>
+      <ul>
+        <li>Medias</li>
+        <li>Gorras</li>
+        <li>Tuki</li>
+      </ul>
+    </div>
+  );
+}
