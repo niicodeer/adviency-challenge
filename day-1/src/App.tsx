ec416c421b9c497a84c4951e25928ac3c93f0a20
Nico Radin <nicoradin.job@gmail.com>
2022-12-06 15:24:44 +0000

    readme

--- day-1/src/App.tsx
+++ day-1/src/App.tsx
@@ -4,12 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Regalos:</h1>
-      <ul>
-        <li>Remera</li>
-        <li>Notebook</li>
-        <li>Silla gamer</li>
-      </ul>
+        <h1>Regalos:</h1>
+        <ul>
+          <li>Remera</li>
+          <li>Notebook</li>
+          <li>Silla gamer</li>
+        </ul>
       </div>
     </div>
   );
