e434ecc6a078e545d664965f90aa2374e0724158
Nico Radin <nicoradin.job@gmail.com>
2022-12-06 14:01:44 +0000

    day-1

---
+++ day-1/src/App.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Regalos:</h1>
+      <ul>
+        <li>Remera</li>
+        <li>Notebook</li>
+        <li>Silla gamer</li>
+      </ul>
+      </div>
+    </div>
+  );
+}
