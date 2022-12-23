6887dc4ce5b4fe8c6fd6a36ba6a02f95f4c906b5
Nico Radin <nicoradin.job@gmail.com>
2022-12-23 01:09:33 +0000

    day-9

---
+++ day-9/src/App.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [giftList, setGiftList] = useState<
+  { giftName: string; giftAmount: number }[]
+  >([]);
+  const [giftName, setGiftName] = useState("");
+  const [giftAmount, setGiftAmount] = useState(1);
+  
+  
+  useEffect(() => {
+    const initialList = JSON.parse(localStorage.getItem('Gift-List') ||"");
+    if(initialList){
+      setGiftList(initialList);
+    }
+  }, []);
+  useEffect(()=>{
+    localStorage.setItem('Gift-List', JSON.stringify(giftList));
+  }, [giftList])
+
+
+  const handleSubmit = () => {
+    let draft = {
+      giftName,
+      giftAmount,
+    };
+    if (draft.giftName.trim().length > 0 && draft.giftAmount > 0) {
+      setGiftList([draft, ...giftList]);
+      setGiftName("");
+      setGiftAmount(1);
+    }
+  };
+
+  const handleDelete = (index: number) => {
+    const filtered = giftList.filter((gift, key) => key != index);
+    setGiftList(filtered);
+  };
+
+  const deleteAll = () => {
+    setGiftList([]);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <form
+          id="formGift"
+          className="container__form"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            type="text"
+            placeholder="Escriba su regalo"
+            autoComplete="off"
+            autoFocus
+            value={giftName}
+            name="giftName"
+            onChange={(e) => setGiftName(e.target.value)}
+          />
+          <input
+            type="number"
+            name="giftAmount"
+            autoComplete="off"
+            min={1}
+            value={giftAmount}
+            onChange={(e) => setGiftAmount(Number(e.target.value))}
+          />
+          <button type="button" onClick={handleSubmit}>
+            Agregar
+          </button>
+        </form>
+        <div className="container__list">
+          <h1>Lista de Regalos:</h1>
+          <ul>
+            {giftList.length == 0 ? (
+              <div style={{ paddingTop: "1em" }}>
+                <p>
+                  No hay regalos en la lista! <br /> ¿Qué esperas? <br /> Agregá
+                  uno! 🎁😀{" "}
+                </p>
+              </div>
+            ) : (
+              giftList.map((gift, index) => (
+                <div className="container__list__item" key={index}>
+                  <div>
+                    <li>{gift.giftName}</li>
+                    <span>{gift.giftAmount}</span>
+                  </div>
+                  <div>
+                    <button type="button" onClick={() => handleDelete(index)}>
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
+              ))
+            )}
+          </ul>
+        </div>
+        <button
+          type="button"
+          className="container__delete-btn"
+          onClick={deleteAll}
+        >
+          Eliminar todo
+        </button>
+      </div>
+    </div>
+  );
+}
